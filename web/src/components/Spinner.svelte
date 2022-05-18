3faa25528f3c90f4d3209773fa554e6cb891277c
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-05-18 19:19:18 +0300

    feat(web): spinner

---
+++ web/src/components/Spinner.svelte
@@ -0,0 +1,22 @@
+<script>
+  import FaSpinner from 'svelte-icons/fa/FaSpinner.svelte'
+</script>
+
+<div class='icon spin'>
+  <FaSpinner/>
+</div>
+
+<style>
+  .spin {
+    animation: spin 1.2s linear infinite;
+  }
+
+  @keyframes spin {
+    fron {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
--- web/src/views/ListingView.svelte
+++ web/src/views/ListingView.svelte
@@ -1,9 +1,23 @@
 <script lang='ts'>
+  import { onMount } from 'svelte';
+
   import Cards from '../components/Cards.svelte';
+  import Spinner from '../components/Spinner.svelte';
+
+  let showSpinner = true;
+
+  onMount(() => {
+    setTimeout(() => showSpinner = false, 5000)
+  });
+
 </script>
 
 <div class='container'>
-  <Cards/>
+  {#if showSpinner}
+    <Spinner/>
+  {:else}
+    <Cards/>
+  {/if}
 </div>
 
 <style>
